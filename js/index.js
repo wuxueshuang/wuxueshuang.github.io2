a9cc373534b3e32766c8883dca4441260518ce42
wuxueshuang <13522709631@163.com>
2017-11-21 17:56:44 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -9,23 +9,39 @@ window.onload = function(){
 		lis[i].style.left = i*25+ "%";
 	}
 	
-	//判断page2Box是否在可视区中
-	/*function isShowInit(){
+
+
+
+	function isShowInit(){
 		let page2Box = document.getElementsByClassName("section-2")[0];
 		//判断page2Box是否在可视区中；
 		function getRect(obj){
 	        return obj.getBoundingClientRect();
 	    }
-	    if(getRect(page2Box).top < document.documentElement.clientHeight-60){
-	        console.log("在可视区域里了1111",getRect(page2Box).top,document.documentElement.clientHeight);
+	    if(Math.floor(getRect(page2Box).top) <= Math.floor(document.documentElement.clientHeight)&&Math.floor(getRect(page2Box).top) > 0){
+	        //console.log("在可视区域里了1111",Math.floor(Math.abs(getRect(page2Box).top)),document.documentElement.clientHeight);
+	        introduce();
+	        //console.log("我执行了");        
 	        return true;
 	    }else{        
-	        console.log("没在可视区域中",getRect(page2Box).top,document.documentElement.clientHeight);
+	        //console.log("没在可视区域中",Math.floor(Math.abs(getRect(page2Box).top)),document.documentElement.clientHeight);
 	        return false;
 	    }
-	}*/
+	}
+	//抖动效果
+	function introduce(){	
+		let iBox = document.getElementById("i-box");
+		let shakeEles = iBox.children;
+		for( let i = 0; i < shakeEles.length; i++ ){
+			shake(shakeEles[i],"left",25);
+			shakeEles[i].onclick = function(){
+				shake(shakeEles[i],"left",25);
+			}
+		}
+	}
 
 
+	let iNow = 0;//记录哈希值
 	//此处引用：鼠标滚轮mousewheel插件
 	!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?module.exports=a:a(jQuery)}(function(a){function b(b){var g=b||window.event,h=i.call(arguments,1),j=0,l=0,m=0,n=0,o=0,p=0;if(b=a.event.fix(g),b.type="mousewheel","detail"in g&&(m=-1*g.detail),"wheelDelta"in g&&(m=g.wheelDelta),"wheelDeltaY"in g&&(m=g.wheelDeltaY),"wheelDeltaX"in g&&(l=-1*g.wheelDeltaX),"axis"in g&&g.axis===g.HORIZONTAL_AXIS&&(l=-1*m,m=0),j=0===m?l:m,"deltaY"in g&&(m=-1*g.deltaY,j=m),"deltaX"in g&&(l=g.deltaX,0===m&&(j=-1*l)),0!==m||0!==l){if(1===g.deltaMode){var q=a.data(this,"mousewheel-line-height");j*=q,m*=q,l*=q}else if(2===g.deltaMode){var r=a.data(this,"mousewheel-page-height");j*=r,m*=r,l*=r}if(n=Math.max(Math.abs(m),Math.abs(l)),(!f||f>n)&&(f=n,d(g,n)&&(f/=40)),d(g,n)&&(j/=40,l/=40,m/=40),j=Math[j>=1?"floor":"ceil"](j/f),l=Math[l>=1?"floor":"ceil"](l/f),m=Math[m>=1?"floor":"ceil"](m/f),k.settings.normalizeOffset&&this.getBoundingClientRect){var s=this.getBoundingClientRect();o=b.clientX-s.left,p=b.clientY-s.top}return b.deltaX=l,b.deltaY=m,b.deltaFactor=f,b.offsetX=o,b.offsetY=p,b.deltaMode=0,h.unshift(b,j,l,m),e&&clearTimeout(e),e=setTimeout(c,200),(a.event.dispatch||a.event.handle).apply(this,h)}}function c(){f=null}function d(a,b){return k.settings.adjustOldDeltas&&"mousewheel"===a.type&&b%120===0}var e,f,g=["wheel","mousewheel","DOMMouseScroll","MozMousePixelScroll"],h="onwheel"in document||document.documentMode>=9?["wheel"]:["mousewheel","DomMouseScroll","MozMousePixelScroll"],i=Array.prototype.slice;if(a.event.fixHooks)for(var j=g.length;j;)a.event.fixHooks[g[--j]]=a.event.mouseHooks;var k=a.event.special.mousewheel={version:"3.1.12",setup:function(){if(this.addEventListener)for(var c=h.length;c;)this.addEventListener(h[--c],b,!1);else this.onmousewheel=b;a.data(this,"mousewheel-line-height",k.getLineHeight(this)),a.data(this,"mousewheel-page-height",k.getPageHeight(this))},teardown:function(){if(this.removeEventListener)for(var c=h.length;c;)this.removeEventListener(h[--c],b,!1);else this.onmousewheel=null;a.removeData(this,"mousewheel-line-height"),a.removeData(this,"mousewheel-page-height")},getLineHeight:function(b){var c=a(b),d=c["offsetParent"in a.fn?"offsetParent":"parent"]();return d.length||(d=a("body")),parseInt(d.css("fontSize"),10)||parseInt(c.css("fontSize"),10)||16},getPageHeight:function(b){return a(b).height()},settings:{adjustOldDeltas:!0,normalizeOffset:!0}};a.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})});
 	var i=0;
@@ -36,14 +52,18 @@ window.onload = function(){
 			$arrow = $('.arrow');
 		
 		/*当前页面赋值*/
-		function up(){i++;if(i==$btn.length){i=0};}
-		function down(){i--;if(i<0){i=$btn.length-1};}
-		//isShowInit();
+		function up(){i++;if(i==$btn.length){i=0};iNow++;window.location.hash=iNow + 1;}
+		function down(){i--;if(i<0){i=$btn.length-1};iNow--;window.location.hash=iNow + 1;}
+
 
 		/*页面滑动*/
 		function run(){
 			$btn.eq(i).addClass('on').siblings().removeClass('on');	
 			$wrap.attr("class","section-wrap").addClass(function() { return "put-section-"+i; }).find('.section').eq(i).find('.title').addClass('active');
+			iNow = i;
+			window.location.hash=iNow + 1;
+			//console.log("huadongle");
+			isShowInit();
 		};
 		
 		/*右侧按钮点击*/
@@ -51,7 +71,6 @@ window.onload = function(){
 			$(this).click(function(){
 				i=index;
 				run();
-				//isShowInit();
 			})
 		});
 		
@@ -69,7 +88,7 @@ window.onload = function(){
 			if(event.deltaY<0) {up()}
 			else{down()}
 			run();
-			//isShowInit();
+			isShowInit();
 			setTimeout(function(){$wrap.one('mousewheel',mouse_)},1000)
 		};
 		
@@ -90,8 +109,7 @@ window.onload = function(){
 			up:up,
 			down:down,
 			run:run,
-			go:go,
-			//isShowInit:isShowInit
+			go:go
 		}
 	});
 
@@ -107,6 +125,8 @@ window.onload = function(){
 			$("item span").css("transform","rotateX(90deg)");
 			$("item span").css("transition",null);
 			i = index;
+			iNow = i;
+			window.location.hash=iNow + 1;
 			$('.section-btn li').eq(i).addClass('on').siblings().removeClass('on');	
 			$('.section-wrap').attr("class","section-wrap").addClass(function() { return "put-section-"+i; }).find('.section').eq(i).find('.title').addClass('active');
 		});
--- js/introduce.js
+++ js/introduce.js
@@ -1,43 +1,41 @@
 'use strict';
 // 项目经验
-/*function isShowInit(){
-	let page2Box = document.getElementsByClassName("section-4")[0];
+//判断page2Box是否在可视区中
+function isShowInit(){
+	let page2Box = document.getElementsByClassName("section-2")[0];
 	//判断page2Box是否在可视区中；
 	function getRect(obj){
         return obj.getBoundingClientRect();
     }
-    if(getRect(page2Box).top < document.documentElement.clientHeight-60){
-        console.log("在可视区域里了1111",getRect(page2Box).top,document.documentElement.clientHeight);
+    if(Math.floor(getRect(page2Box).top) <= Math.floor(document.documentElement.clientHeight)&&Math.floor(getRect(page2Box).top) > 0){
+        //console.log("在可视区域里了1111",Math.floor(Math.abs(getRect(page2Box).top)),document.documentElement.clientHeight);
+        introduce();        
         return true;
     }else{        
-        console.log("没在可视区域中",getRect(page2Box).top,document.documentElement.clientHeight);
+        //console.log("没在可视区域中",Math.floor(Math.abs(getRect(page2Box).top)),document.documentElement.clientHeight);
         return false;
     }
 }
-//页面一进来先执行一次；
-//isShowInit();
-//在浏览器页面滚动时都要执行启动函数：
-window.onscroll = window.onresize = isShowInit; 
-
-mousewheel(document,isShowInit,isShowInit)*/
-
-function introduce(){
-	
-
+//抖动效果
+function introduce(){	
 	let iBox = document.getElementById("i-box");
-	//console.log(iBox);
 	let shakeEles = iBox.children;
-	//console.log(shakeEles);
 	for( let i = 0; i < shakeEles.length; i++ ){
 		shake(shakeEles[i],"left",25);
 		shakeEles[i].onclick = function(){
 			shake(shakeEles[i],"left",25);
-			//isShowInit();
 		}
 	}
 }
 
-introduce();
+//页面一进来先执行一次；
+isShowInit();
+//在浏览器页面滚动时都要执行启动函数：
+window.onscroll = window.onresize = isShowInit;
+
+mousewheel(document,isShowInit,isShowInit)
+
+
 
 
 
